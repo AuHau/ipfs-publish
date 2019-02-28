8bc6eb14c579c4d6a0e97e08a4c704625b695b64
Adam Uhlir <uhlir.a@gmail.com>
2019-02-28 13:34:15 -0800

    Testing script support different hostnames

--- github_test.sh
+++ github_test.sh
@@ -2,9 +2,10 @@
 
 repo_name=$1
 secret=$2
+url=${3-http://localhost:8000}
 
 data="{\"ref\": \"\"}"
 
 sig=$(echo -n "${data}" | openssl dgst -sha1 -hmac "${secret}" | awk '{print "X-Hub-Signature: sha1="$1}')
 
-curl -X POST -H "X-GitHub-Event: push" -H "Content-Type: application/json" -H "${sig}" --data "${data}" http://localhost:8000/publish/${repo_name}
+curl -X POST -H "X-GitHub-Event: push" -H "Content-Type: application/json" -H "${sig}" --data "${data}" ${url}/publish/${repo_name}
